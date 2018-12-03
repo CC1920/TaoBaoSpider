2a0bb4a5c1032ae85eae80df3303fd39569e7d24
张生 <qiqi1931@163.com>
2018-12-03 20:57:12 +0800

    Update urtils.py

--- daxi/daxi/urtils.py
+++ daxi/daxi/urtils.py
@@ -4,9 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-
-
-
 def ease_out_expo(x):
     if x == 1:
         return 1
@@ -47,23 +44,15 @@ def drag_and_drop(url):
     button = driver.find_element_by_css_selector('#nc_1_n1z')
     button_location = button.location
     print(button_location)  # 滑块位置的x y 坐标 {'x': 314, 'y': 262}
-
-
     # x_ 是滑动条的长度
     # y 是滑块的 y 坐标
-
-
-    # 这个为什么这样写？ 看不懂
-
     x_location = span_background_size['width']
     y_location = button_location['y']
     time.sleep(2)
 
-
     # 现在要给Y 写一个循环， 写一个简单的循环
     # 这个能滑过去，但是速度很快
     # ActionChains(driver).drag_and_drop_by_offset(button, x_location, y_location).perform()
-
 
     y = y_location
     tracks = get_tracks(y, 50, ease_out_expo)
